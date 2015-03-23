f210cc3fe716c7cfba000c76daf7280095d45d19
Gareth Tan <garethwtan@gmail.com>
2015-03-23 11:42:28 -0400

    Add error handling.

--- index.js
+++ index.js
@@ -6,20 +6,28 @@ var through     = require('through');
 module.exports = function(file) {
   var data = "";
   var header = "var regeneratorRuntime = require('regenerator/runtime');\n";
-  var stream = through(write, end);
-
-  return stream;
 
   function write(buf) {
     data += buf;
   }
 
   function end() {
-    var rdata = regenerator.compile(data).code;
+    try {
+      var rdata = regenerator.compile(data).code;
+    } catch (e) {
+      stream.emit("error", e);
+      stream.queue(null);
+      return;
+    }
+
     if (rdata !== data) {
       rdata = header + data;
     }
     stream.queue(rdata);
     stream.queue(null);
   }
+
+  var stream = through(write, end);
+
+  return stream;
 }
